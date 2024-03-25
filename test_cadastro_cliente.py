08ca587c3de24f909d4e75fa4a43216de261ee69
ana <anajuliasantinoni15@gmail.com>
2024-03-25 10:54:51 -0300

    erro falta de parametros

--- test_cadastro_cliente.py
+++ test_cadastro_cliente.py
@@ -1,4 +1,7 @@
-def test_cliente_cadastrado();
+from Cliente import Cliente
+from CadastroCliente import CadastroCliente
+
+def test_cliente_cadastrado():
     cliente = Cliente()
     cadastro_cliente = CadastroCliente()
     resposta = cadastro_cliente.cadastrar_cliente(cliente)
