f6f2a25ce7fc489fd9894dcbe1d919f7df9ac9fb
ana <anajuliasantinoni15@gmail.com>
2024-03-25 12:06:05 -0300

    if de email invalido

--- CadastroCliente.py
+++ CadastroCliente.py
@@ -6,7 +6,11 @@ class CadastroCliente:
         if cliente.idade < 18:
             return "Cliente menor de idade"
         
+        if not '@' in cliente.email:
+            return 'Email invalido'
+
         self.clientes_cadastrados.append(cliente)
 
-        if len(self.clientes_cadastrados) > 0: #conferir se esta realmente cadastrando.
+        # conferir se esta realmente cadastrando.
+        if len(self.clientes_cadastrados) > 0:
             return "Cadastrado com sucesso"
